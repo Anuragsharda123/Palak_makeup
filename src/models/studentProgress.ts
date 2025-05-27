5b713719a1d23df99dfac1f89f8b1a767b290050
unknown <anuragsharda131@gmail.com>
2025-05-27 17:02:14 +0530

    Dev_27-5-2025

--- src/models/studentProgress.ts
+++ src/models/studentProgress.ts
@@ -1,45 +1,66 @@
 import sequelize from "../config/db";
 import { DataTypes, Model } from "sequelize";
 import { v4 as UUIDV4 } from "uuid";
-import Course from "./course";
 import Video from "./video";
 import User from "./user";
 
-class studentProgress extends Model{
-    public uuid!: string;
-    public studentId!: string;
-    public videoId!: string;
-    public progess!: number;
-    public iscompleted!: boolean;
+class studentProgress extends Model {
+  public uuid!: string;
+  public studentId!: string;
+  public videoId!: string;
+  public progess!: number;
+  public iscompleted!: boolean;
 }
 
-studentProgress.init({
-    uuid:{
-        type: DataTypes.UUID,
-        defaultValue: UUIDV4,
-        primaryKey: true,
-        allowNull: false
+studentProgress.init(
+  {
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+      allowNull: false,
     },
-    progress:{
-        type: DataTypes.DECIMAL,
-        defaultValue: 0
+    progress: {
+      type: DataTypes.DECIMAL,
+      defaultValue: 0,
     },
-    isCompleted:{
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    }
-},{
+    isCompleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
     sequelize,
     modelName: "student_progress",
     paranoid: true,
-    timestamps: true
+    timestamps: true,
+  }
+);
+
+Video.hasMany(studentProgress, {
+  foreignKey: "videoId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+  as: "progressVideo",
+});
+studentProgress.belongsTo(Video, {
+  foreignKey: "videoId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+  as: "progressVideo",
 });
 
-Video.hasMany(studentProgress, { foreignKey:"videoId", onDelete: "CASCADE", onUpdate: "CASCADE", as: "progressVideo" });
-studentProgress.belongsTo(Video, { foreignKey:"videoId", onDelete: "CASCADE", onUpdate:"CASCADE", as:"progressVideo"});
-
-User.hasMany(studentProgress, { foreignKey:"studentId", onDelete: "CASCADE", onUpdate:"CASCADE", as:"progressUser"});
-studentProgress.belongsTo(User, { foreignKey:"studentId", onDelete: "CASCADE", onUpdate:"CASCADE", as:"progressUser"});
-
+User.hasMany(studentProgress, {
+  foreignKey: "studentId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+  as: "progressUser",
+});
+studentProgress.belongsTo(User, {
+  foreignKey: "studentId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+  as: "progressUser",
+});
 
 export default studentProgress;
